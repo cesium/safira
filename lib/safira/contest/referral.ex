c00b54992208cf3ff605647c38fa1ce3665237c2
ZePedroResende <j.p.resende68@gmail.com>
2018-07-07 18:37:27 +0100

    correct schema

--- lib/safira/contest/referral.ex
+++ lib/safira/contest/referral.ex
@@ -3,7 +3,8 @@ defmodule Safira.Contest.Referral do
   import Ecto.Changeset
   alias Safira.Contest.Badge
 
-
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @derive {Phoenix.Param, key: :id}
   schema "referrals" do
     field :available, :boolean, default: true
     belongs_to :badge, Badge
