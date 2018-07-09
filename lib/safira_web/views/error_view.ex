ab7b5bb98046e9156882c9f6cb970f7f4937ec21
0urobor0s <lhome.gamer@gmail.com>
2018-07-09 17:14:37 +0100

    .

--- lib/safira_web/views/error_view.ex
+++ lib/safira_web/views/error_view.ex
@@ -10,15 +10,16 @@ defmodule SafiraWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
-  end
 
   def render("404.json", _assigns) do
-    %{errors: %{detail: "Endpoint not found"}}
+    %{errors: %{detail: "Endpoint Not Found"}}
   end
 
   def render("500.json", _assigns) do
     %{errors: %{detail: "Internal Server Error"}}
+  end
+
+  def template_not_found(template, _assigns) do
+    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 end
