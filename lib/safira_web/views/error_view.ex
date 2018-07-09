1ad93fb9b891cb0fa07c5cf98e39fa673abcc64f
0urobor0s <lhome.gamer@gmail.com>
2018-07-09 17:00:59 +0100

    Handle for custom errors

--- lib/safira_web/views/error_view.ex
+++ lib/safira_web/views/error_view.ex
@@ -13,4 +13,12 @@ defmodule SafiraWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Endpoint not found"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal Server Error"}}
+  end
 end
