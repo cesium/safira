459ab4b2c0a6ab4e05bb7d1701caea8293329cde
0urobor0s <lhome.gamer@gmail.com>
2018-07-25 17:09:08 +0100

    Add company resource

--- lib/safira/accounts/accounts.ex
+++ lib/safira/accounts/accounts.ex
@@ -8,6 +8,7 @@ defmodule Safira.Accounts do
 
   alias Safira.Accounts.User
   alias Safira.Accounts.Attendee
+  alias Safira.Accounts.Company
 
   alias Safira.Guardian
 
@@ -83,6 +84,32 @@ defmodule Safira.Accounts do
     Attendee.changeset(attendee, %{})
   end
 
+  def list_companies do
+    Repo.all(Company)
+  end
+
+  def get_company!(id), do: Repo.get!(Company, id)
+
+  def create_company(attrs \\ %{}) do
+    %Company{}
+    |> Company.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_company(%Company{} = company, attrs) do
+    company
+    |> Company.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_company(%Company{} = company) do
+    Repo.delete(company)
+  end
+
+  def change_company(%Company{} = company) do
+    Company.changeset(company, %{})
+  end
+
   def token_sign_in(email, password) do
     case email_password_auth(email, password) do
       {:ok, user} ->
@@ -113,101 +140,5 @@ defmodule Safira.Accounts do
     else
       {:error, :invalid_password}
     end
-  end
-
-  alias Safira.Accounts.Company
-
-  @doc """
-  Returns the list of companies.
-
-  ## Examples
-
-      iex> list_companies()
-      [%Company{}, ...]
-
-  """
-  def list_companies do
-    Repo.all(Company)
-  end
-
-  @doc """
-  Gets a single company.
-
-  Raises `Ecto.NoResultsError` if the Company does not exist.
-
-  ## Examples
-
-      iex> get_company!(123)
-      %Company{}
-
-      iex> get_company!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_company!(id), do: Repo.get!(Company, id)
-
-  @doc """
-  Creates a company.
-
-  ## Examples
-
-      iex> create_company(%{field: value})
-      {:ok, %Company{}}
-
-      iex> create_company(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_company(attrs \\ %{}) do
-    %Company{}
-    |> Company.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a company.
-
-  ## Examples
-
-      iex> update_company(company, %{field: new_value})
-      {:ok, %Company{}}
-
-      iex> update_company(company, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_company(%Company{} = company, attrs) do
-    company
-    |> Company.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Company.
-
-  ## Examples
-
-      iex> delete_company(company)
-      {:ok, %Company{}}
-
-      iex> delete_company(company)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_company(%Company{} = company) do
-    Repo.delete(company)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking company changes.
-
-  ## Examples
-
-      iex> change_company(company)
-      %Ecto.Changeset{source: %Company{}}
-
-  """
-  def change_company(%Company{} = company) do
-    Company.changeset(company, %{})
   end
 end
