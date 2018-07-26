f591725228778986d46e8446a5d5b66bfcad8b20
ZePedroResende <j.p.resende68@gmail.com>
2018-07-27 00:08:24 +0100

    Add redeem resource
    Co-authored-by: ZePedroResende <j.p.resende68@gmail.com>
    Co-authored-by: Daniel Tinoco <0urobor0s@users.noreply.github.com>

--- lib/safira/contest/contest.ex
+++ lib/safira/contest/contest.ex
@@ -2,7 +2,7 @@ defmodule Safira.Contest do
 
   import Ecto.Query, warn: false
   alias Safira.Repo
-
+  alias Safira.Contest.Redeem
   alias Safira.Contest.Badge
 
   def list_badges do
@@ -59,98 +59,28 @@ defmodule Safira.Contest do
     Referral.changeset(referral, %{})
   end
 
-  alias Safira.Contest.Redeem
-
-  @doc """
-  Returns the list of redeems.
-
-  ## Examples
-
-      iex> list_redeems()
-      [%Redeem{}, ...]
-
-  """
   def list_redeems do
     Repo.all(Redeem)
   end
 
-  @doc """
-  Gets a single redeem.
-
-  Raises `Ecto.NoResultsError` if the Redeem does not exist.
-
-  ## Examples
-
-      iex> get_redeem!(123)
-      %Redeem{}
-
-      iex> get_redeem!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_redeem!(id), do: Repo.get!(Redeem, id)
 
-  @doc """
-  Creates a redeem.
-
-  ## Examples
-
-      iex> create_redeem(%{field: value})
-      {:ok, %Redeem{}}
-
-      iex> create_redeem(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_redeem(attrs \\ %{}) do
     %Redeem{}
     |> Redeem.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a redeem.
-
-  ## Examples
-
-      iex> update_redeem(redeem, %{field: new_value})
-      {:ok, %Redeem{}}
-
-      iex> update_redeem(redeem, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_redeem(%Redeem{} = redeem, attrs) do
     redeem
     |> Redeem.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Redeem.
-
-  ## Examples
-
-      iex> delete_redeem(redeem)
-      {:ok, %Redeem{}}
-
-      iex> delete_redeem(redeem)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_redeem(%Redeem{} = redeem) do
     Repo.delete(redeem)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking redeem changes.
-
-  ## Examples
-
-      iex> change_redeem(redeem)
-      %Ecto.Changeset{source: %Redeem{}}
-
-  """
   def change_redeem(%Redeem{} = redeem) do
     Redeem.changeset(redeem, %{})
   end
