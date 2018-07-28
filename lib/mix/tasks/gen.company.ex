0fe98441a25c30e64892979df3ceea0a26340754
Daniel Tinoco <0urobor0s@users.noreply.github.com>
2018-07-28 16:23:33 +0100

    Task generate company user (#17)

---
+++ lib/mix/tasks/gen.company.ex
@@ -0,0 +1,44 @@
+defmodule Mix.Tasks.Gen.Company do
+  use Mix.Task
+  alias Safira.Accounts
+
+  @domain "seium.org"
+
+  def run(args) do
+    cond do
+      length(args) != 3 ->
+        Mix.shell.info "Needs to receive a Company name, sponsorship and badge_id."
+      args |> List.last |> String.to_integer < 0 ->
+        Mix.shell.info "Number of badge_id needs to be above 0."
+      true ->
+        args |> create
+    end
+  end
+
+  defp create(args) do
+    Mix.Task.run "app.start"
+
+    email = Enum.join([Enum.at(args,0), @domain], "@") |> String.downcase
+    password = random_string(8)
+    user = Accounts.create_user(%{
+      "email" => email,
+      "password" => password,
+      "password_confirmation" => password
+    })
+
+    Accounts.create_company(%{
+      "name" => Enum.at(args,0),
+      "sponsorship" => Enum.at(args,0),
+      "badge_id" => args |> List.last |> String.to_integer,
+      "user" => user
+    })
+
+    IO.puts "#{email}:#{password}"
+  end
+
+  defp random_string(length) do
+    :crypto.strong_rand_bytes(length)
+    |> Base.url_encode64
+    |> binary_part(0, length)
+  end
+end
--- lib/mix/tasks/gen.referrals.ex
+++ lib/mix/tasks/gen.referrals.ex
@@ -6,7 +6,7 @@ defmodule Mix.Tasks.Gen.Referrals do
     cond do
       length(args) != 2 ->
         Mix.shell.info "Needs to receive badge id and number of refarrels"
-      args |> List.last|> String.to_integer <= 0 ->
+      args |> List.last |> String.to_integer <= 0 ->
         Mix.shell.info "Number of refarrels needs to be above 0."
       true ->
         args |> Enum.map(&String.to_integer/1) |> create
