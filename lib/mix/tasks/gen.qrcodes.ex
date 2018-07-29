4ae7ef27e3f9448c6735216e2e5df4bcc95f706e
0urobor0s <lhome.gamer@gmail.com>
2018-07-29 23:52:49 +0100

    .

--- lib/mix/tasks/gen.qrcodes.ex
+++ lib/mix/tasks/gen.qrcodes.ex
@@ -5,11 +5,10 @@ defmodule Mix.Tasks.Gen.QrCodes do
   alias Safira.Contest
 
   @arg_types ["attendees", "referrals"]
-  @url "https://intra.seium.org/"
 
   def run(args) do
     cond do
-      length(args) != 1 ->
+      args |> List.first |> (&Enum.member?(@arg_types, &1)).() |> Kernel.not ->
         Mix.shell.info "Needs to receive attendees or referrals."
       true ->
         args |> List.first |> create
@@ -19,17 +18,19 @@ defmodule Mix.Tasks.Gen.QrCodes do
   defp create(arg) when arg in @arg_types do
     Mix.Task.run "app.start"
 
+    url = System.get_env("FRONTEND_URL")
+
     if arg == "attendees" do
       Enum.each Accounts.list_attendees, fn(attendee) ->
         QrCodeSvg.generate(
-          "#{@url}attendees/#{attendee.id}", 
+          "#{url}attendees/#{attendee.id}", 
           "attendee_#{attendee.id}.svg"
         )
       end
     else
       Enum.each Contest.list_referrals, fn(referral) ->
         QrCodeSvg.generate(
-          "#{@url}referrals/#{referral.id}", 
+          "#{url}referrals/#{referral.id}", 
           "referral_#{referral.id}.svg"
         )
       end
