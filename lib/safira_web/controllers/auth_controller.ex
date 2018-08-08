8feb0598c9a80a10cfed2ea04afb0f6f53a2c351
ZePedroResende <j.p.resende68@gmail.com>
2018-08-08 18:01:25 +0100

    add attendee

--- lib/safira_web/controllers/auth_controller.ex
+++ lib/safira_web/controllers/auth_controller.ex
@@ -3,13 +3,14 @@ defmodule SafiraWeb.AuthController do
 
   alias Safira.Auth
   alias Safira.Guardian
+  alias Safira.Accounts
 
   action_fallback SafiraWeb.FallbackController
 
   def sign_up(conn, %{"user" => user_params}) do
     with {:ok, multi} <- Auth.create_user_uuid(user_params),
          {:ok, token, _claims} <- Guardian.encode_and_sign(multi.user) do
-      conn 
+      conn
       |> render("jwt.json", jwt: token)
     end
   end
@@ -19,6 +20,17 @@ defmodule SafiraWeb.AuthController do
     render(conn, "user.json", user: user)
   end
 
+  def attendee(conn, _params) do
+    user = Guardian.Plug.current_resource(conn)
+    user_preload = Safira.Accounts.get_user_preload!(user.id)
+    case is_nil user_preload do
+      true ->
+        {:error, :unauthorized}
+      _ ->
+        render(conn, "attendee.json", user: user_preload)
+    end
+  end
+
   def sign_in(conn, %{"email" => email, "password" => password}) do
     case Auth.token_sign_in(email, password) do
       {:ok, token, _claims} ->
@@ -26,5 +38,5 @@ defmodule SafiraWeb.AuthController do
       _ ->
         {:error, :unauthorized}
     end
-  end  
+  end
 end
--- lib/safira_web/views/auth_view.ex
+++ lib/safira_web/views/auth_view.ex
@@ -5,7 +5,13 @@ defmodule SafiraWeb.AuthView do
     %{id: user.id,
       email: user.email}
   end
-  
+
+  def render("attendee.json", %{user: user}) do
+    %{id: user.attendee.id,
+      nick: user.attendee.nickname,
+      email: user.email}
+  end
+
   def render("jwt.json", %{jwt: jwt}) do
     %{jwt: jwt}
   end
