1596fe141ed2597d4c3b2ed169ba667b0bc21128
0urobor0s <lhome.gamer@gmail.com>
2018-08-10 15:17:06 +0100

    Configure deploy

--- lib/safira_web/channels/user_socket.ex
+++ lib/safira_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule SafiraWeb.UserSocket do
   # channel "room:*", SafiraWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
