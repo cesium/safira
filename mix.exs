ecb74763a43aa8d92566c46bbbcdef1f91440c5e
0urobor0s <lhome.gamer@gmail.com>
2018-08-10 15:22:20 +0100

    Configure deploy

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Safira.Mixfile do
     [
       app: :safira,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
