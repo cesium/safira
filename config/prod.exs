80023346783465af9acdce2ec1eb9d01cfe71e79
0urobor0s <lhome.gamer@gmail.com>
2018-09-13 12:25:58 +0100

    Don't need static stuff yet

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :safira, SafiraWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "sei19-safira.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+# cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
