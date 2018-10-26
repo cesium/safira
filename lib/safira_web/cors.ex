1e6422ed03f2416b52a950eb8958fa9e1868d8fe
Daniel Tinoco <0urobor0s@users.noreply.github.com>
2018-10-26 21:43:30 +0100

    Add more headers for CORS (#35)

--- lib/safira_web/cors.ex
+++ lib/safira_web/cors.ex
@@ -1,6 +1,8 @@
 defmodule SafiraWeb.CORS do
   use Corsica.Router,
     origins: System.get_env("FRONTEND_URL"),
+    log: [rejected: :error, invalid: :warn, accepted: :debug],
+    allow_headers: :all,
     allow_credentials: true,
     max_age: 600
 
