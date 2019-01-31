2d44211dcde8863a72b046ccc0db2af454f255a9
ZePedroResende <j.p.resende68@gmail.com>
2019-01-31 06:12:05 +0000

    fix aws

--- lib/safira/web/uploaders/avatar.ex
+++ lib/safira/web/uploaders/avatar.ex
@@ -44,7 +44,7 @@ defmodule Safira.Avatar do
     |> String.split(".") 
     |> List.last 
     |> String.downcase
-   "#{struct}/avatars/#{scope.id}"
+   "/#{System.get_env("S3_BUCKET")}/#{struct}/avatars/#{scope.id}"
   end
 
   # Provide a default URL if there hasn't been a file uploaded
