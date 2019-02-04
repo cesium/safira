159454d29694972b8f99f5a5cac99cb33c90e90e
ZePedroResende <j.p.resende68@gmail.com>
2019-02-04 20:02:25 +0000

    Add attendee to Badge view

--- lib/safira/contest/contest.ex
+++ lib/safira/contest/contest.ex
@@ -32,6 +32,12 @@ defmodule Safira.Contest do
 
   def get_badge!(id), do: Repo.get!(Badge, id)
 
+
+  def get_badge_preload!(id) do
+    Repo.get!(Badge, id)
+    |> Repo.preload(:attendees)
+  end
+
   def create_badge(attrs \\ %{}) do
     %Badge{}
     |> Badge.changeset(attrs)
--- lib/safira_web/views/badge_view.ex
+++ lib/safira_web/views/badge_view.ex
@@ -18,7 +18,17 @@ defmodule SafiraWeb.BadgeView do
       avatar: Avatar.url({badge.avatar, badge}, :original),
       begin: badge.begin,
       end: badge.end,
-      type: badge.type
+      type: badge.type,
+      attendees: render_many(badge.attendees, BadgeView, "attendee.json" ) 
     }
   end
+
+  def render("attendee.json", %{badge: attendee}) do
+    %{id: attendee.id,
+      nickname: attendee.nickname,
+      avatar: Avatar.url({attendee.avatar, attendee}, :original),
+      volunteer: attendee.volunteer
+    }
+  end
+
 end
