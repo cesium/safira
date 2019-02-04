99fe43053f6751fc18646cbf2c47a58cb7f4f148
ZePedroResende <j.p.resende68@gmail.com>
2019-02-04 10:29:40 +0000

    Fix email fmt

--- lib/safira/accounts/user.ex
+++ lib/safira/accounts/user.ex
@@ -26,6 +26,7 @@ defmodule Safira.Accounts.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :password, :password_confirmation])
+    |> downcase_email
     |> validate_required([:email, :password, :password_confirmation])
     |> validate_length(:email, min: 5, max: 255)
     |> validate_format(:email, ~r/\A[^@\s]+@[^@\s]+\z/)
@@ -42,5 +43,9 @@ defmodule Safira.Accounts.User do
       _ ->
         changeset
     end
+  end
+
+  defp downcase_email(changeset) do
+    update_change(changeset, :email, &String.downcase/1)
   end
 end
