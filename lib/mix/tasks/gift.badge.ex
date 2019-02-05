e83ccacc9d1e3874e7a309443e960a52cf3f74cb
0urobor0s <lhome.gamer@gmail.com>
2019-02-05 10:00:57 +0000

    Drop the hammer

--- lib/mix/tasks/gift.badge.ex
+++ lib/mix/tasks/gift.badge.ex
@@ -22,8 +22,37 @@ defmodule Mix.Tasks.Gift.Badge do
     cond do
       is_valid_uuid(Enum.at(args, 0)) ->
         give_by_uuid(args)
+      is_valid_email(Enum.at(args, 0)) ->
+        give_by_email(args)
       File.regular?(Enum.at(args, 0)) ->
         give_by_file(args)
+    end
+  end
+
+  defp is_valid_email(email) do
+    case valid_email(email |> String.trim) do
+      false -> false
+      true -> is_register_email(email |> String.trim)
+    end
+  end
+
+  defp is_register_email(email) do
+    user = Accounts.get_user_preload_email!(email)
+    not is_nil user.attendee
+  end
+
+  defp valid_email(email) do
+    email
+    |> String.trim
+    |> String.match?(~r/\A[^@\s]+@[^@\s]+\z/)
+  end
+
+  defp give_by_email(args) do
+    badge_id = List.last(args) |> String.to_integer
+    if exists_badge(badge_id) do
+      give_email(Enum.at(args, 0), badge_id)
+    else
+      Mix.shell.info "Badge_id needs to be valid."
     end
   end
 
@@ -32,7 +61,7 @@ defmodule Mix.Tasks.Gift.Badge do
     if exists_badge(badge_id) do
       Enum.at(args, 0)
       |> File.stream!
-      |> Enum.map(fn x -> give(x |> String.trim, badge_id) end)
+      |> Enum.map(fn x -> give_check(x |> String.trim, badge_id) end)
     else
       Mix.shell.info "Badge_id needs to be valid."
     end
@@ -44,6 +73,14 @@ defmodule Mix.Tasks.Gift.Badge do
       :error -> false
     end
   end
+
+  defp is_valid_uuid?(arg) do
+    case Ecto.UUID.cast(arg) do
+      {:ok, _} -> true
+      :error -> false
+    end
+  end
+
 
   defp is_register(id) do
     attendee = Accounts.get_attendee!(id)
@@ -68,10 +105,25 @@ defmodule Mix.Tasks.Gift.Badge do
 
   defp give(attendee_id, badge_id) do
     Contest.create_redeem(
-      %{attendee_id: attendee_id, 
-        manager_id: 1, 
+      %{attendee_id: attendee_id,
+        manager_id: 1,
         badge_id: badge_id
       }
     )
   end
+
+  defp give_email(email, badge_id) do
+    user = Accounts.get_user_preload_email!(email)
+    give(user.attendee.id, badge_id)
+  end
+
+  defp give_check(input, badge_id) do
+    cond do
+      is_valid_uuid?(input) ->
+        give(input, badge_id)
+      valid_email(input) ->
+        give_email(input, badge_id)
+    end
+  end
+
 end
