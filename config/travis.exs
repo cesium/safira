d638cadca82155bfa2779d7a9e516879be562726
Daniel Tinoco <0urobor0s@users.noreply.github.com>
2019-10-02 14:46:07 +0100

    Setup CI (#100)

---
+++ config/travis.exs
@@ -0,0 +1,27 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :safira, SafiraWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :safira, Safira.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "safira_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# Fast testing only
+config :bcrypt_elixir, :log_rounds, 4
+
+# Guardian config
+config :safira, Safira.Guardian,
+  issuer: "safira",
+  secret_key: "BnJosu+UxrCR70RWM4dhDJz2bH34D+wTbLcu7R9siQWGr8uGmB8k+ClnAEw3EkVQ"
