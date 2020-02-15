aaae36dfc1be4a688191f353d18e890eeec7daf6
joaop21 <jpsilva9898@gmail.com>
2020-02-15 17:38:48 +0000

    Remove dumb variable

--- lib/mix/tasks/export.attendees.ex
+++ lib/mix/tasks/export.attendees.ex
@@ -3,10 +3,10 @@ defmodule Mix.Tasks.Export.Attendees do
   
     def run(_) do
       Mix.Task.run "app.start"
-      attendees = Safira.Accounts.list_active_attendees
-      Enum.each(attendees, fn a -> 
-        user = Safira.Accounts.get_user!(a.user_id)
-        Mix.shell.info "#{a.id},#{a.name},#{user.email}"
-      end)
+      Safira.Accounts.list_active_attendees
+      |> Enum.each( fn a -> 
+          user = Safira.Accounts.get_user!(a.user_id)
+          Mix.shell.info "#{a.id},#{a.name},#{user.email}"
+         end)
     end
   end
