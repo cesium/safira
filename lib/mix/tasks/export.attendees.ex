30c696f44d8d7f7ba6f70a8e646bff268d4b0ae4
joaop21 <jpsilva9898@gmail.com>
2020-02-15 18:53:31 +0000

    Add message

--- lib/mix/tasks/export.attendees.ex
+++ lib/mix/tasks/export.attendees.ex
@@ -3,9 +3,10 @@ defmodule Mix.Tasks.Export.Attendees do
   
     def run(_) do
       Mix.Task.run "app.start"
+      IO.puts "UUID,Name,Email"
       Safira.Accounts.list_active_attendees
       |> Enum.each( fn a -> 
-          Mix.shell.info "#{a.id},#{a.name},#{Safira.Accounts.get_user!(a.user_id).email}"
+        IO.puts "#{a.id},#{a.name},#{Safira.Accounts.get_user!(a.user_id).email}"
          end)
     end
   end
