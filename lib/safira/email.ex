b0f257be11811b2a87c539137b2892a35ce18359
Daniel Tinoco <0urobor0s@users.noreply.github.com>
2020-02-18 22:02:43 +0000

    Change frontend password reset (#115)

--- lib/safira/email.ex
+++ lib/safira/email.ex
@@ -8,7 +8,7 @@ defmodule Safira.Email do
     |> to(to_email)
     |> from(System.get_env("FROM_EMAIL"))
     |> subject("Reset Password Instructions")
-    |> text_body("Please visit #{System.get_env("FRONTEND_URL")}/password/edit?token=#{token} to reset your password")
+    |> text_body("Please visit #{System.get_env("FRONTEND_URL")}/reset?token=#{token} to reset your password")
   end
 
   def send_password_email(to_email, token) do
@@ -16,6 +16,6 @@ defmodule Safira.Email do
     |> to(to_email)
     |> from(System.get_env("FROM_EMAIL"))
     |> subject("Finish account registration")
-    |> text_body("Please visit #{System.get_env("FRONTEND_URL")}/password/edit?token=#{token} to finish your account registration")
+    |> text_body("Please visit #{System.get_env("FRONTEND_URL")}/reset?token=#{token} to finish your account registration")
   end
 end
