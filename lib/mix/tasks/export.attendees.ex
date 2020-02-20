072c739705f77f610197abc0cdac61bef6ced605
0urobor0s <0urobor0s@users.noreply.github.com>
2020-02-20 02:45:50 +0000

    Export attendee info

--- lib/mix/tasks/export.attendees.ex
+++ lib/mix/tasks/export.attendees.ex
@@ -1,15 +1,45 @@
 defmodule Mix.Tasks.Export.Attendees do
-    use Mix.Task
-  
-    def run(_) do
-      Mix.Task.run "app.start"
+  use Mix.Task
 
-      IO.puts "uuid,name,email,volunteer"
-      Enum.each(
-        Safira.Accounts.list_active_attendees,
-        fn a -> 
-          IO.puts "#{a.id},#{a.name},#{Safira.Accounts.get_user!(a.user_id).email},#{a.volunteer}"
-        end
-      )
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    IO.puts("uuid,name,email,volunteer,food,housing")
+
+    Enum.each(
+      Safira.Accounts.list_active_attendees(),
+      fn a ->
+        IO.puts(csv_io(a, food(a.badges), housing(a.badges)))
+      end
+    )
+  end
+
+  defp food(badges) do
+    badges
+    |> Enum.map(fn b -> b.name end)
+    |> Enum.member?("Jantar domingo")
+  end
+
+  defp housing(badges) do
+    bs =
+      badges
+      |> Enum.map(fn b -> b.name end)
+
+    cond do
+      Enum.member?(bs, "Alojamento D. Maria II") ->
+        "Alojamento D. Maria II"
+
+      Enum.member?(bs, "Alojamento Alberto Sampaio") ->
+        "Alojamento Alberto Sampaio"
+
+      true ->
+        "Não tem"
     end
   end
+
+  defp csv_io(attendee, food, housing) do
+    "#{attendee.id},#{attendee.name},#{Safira.Accounts.get_user!(attendee.user_id).email},#{
+      attendee.volunteer
+    },#{food},#{housing}"
+  end
+end
