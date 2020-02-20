f1c55c2ebfc23798abe16f6e3e296686748b75c7
0urobor0s <0urobor0s@users.noreply.github.com>
2020-02-20 22:36:39 +0000

    Gen company

--- lib/mix/tasks/gen.company.ex
+++ lib/mix/tasks/gen.company.ex
@@ -7,19 +7,22 @@ defmodule Mix.Tasks.Gen.Company do
   def run(args) do
     cond do
       length(args) != 3 ->
-        Mix.shell.info "Needs to receive a Company name, sponsorship and badge_id."
-      args |> List.last |> String.to_integer < 0 ->
-        Mix.shell.info "Number of badge_id needs to be above 0."
+        Mix.shell().info("Needs to receive a Company name, sponsorship and badge_id.")
+
+      args |> List.last() |> String.to_integer() < 0 ->
+        Mix.shell().info("Number of badge_id needs to be above 0.")
+
       true ->
         args |> create
     end
   end
 
   defp create(args) do
-    Mix.Task.run "app.start"
+    Mix.Task.run("app.start")
 
-    email = Enum.join([Enum.at(args,0), @domain], "@") |> String.downcase
+    email = Enum.join([Enum.at(args, 0), @domain], "@") |> String.downcase()
     password = random_string(8)
+
     user = %{
       "email" => email,
       "password" => password,
@@ -27,18 +30,18 @@ defmodule Mix.Tasks.Gen.Company do
     }
 
     Accounts.create_company(%{
-      "name" => Enum.at(args,0),
-      "sponsorship" => Enum.at(args,0),
-      "badge_id" => args |> List.last |> String.to_integer,
+      "name" => Enum.at(args, 0),
+      "sponsorship" => Enum.at(args, 1),
+      "badge_id" => args |> List.last() |> String.to_integer(),
       "user" => user
     })
 
-    IO.puts "#{email}:#{password}"
+    IO.puts("#{email}:#{password}")
   end
 
   defp random_string(length) do
     :crypto.strong_rand_bytes(length)
-    |> Base.url_encode64
+    |> Base.url_encode64()
     |> binary_part(0, length)
   end
 end
