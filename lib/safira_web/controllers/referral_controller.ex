e3b09c5f59e318d28bba2a2240cc235fa22b920a
0urobor0s <0urobor0s@users.noreply.github.com>
2020-02-20 23:15:29 +0000

    Referral controller

--- lib/safira_web/controllers/referral_controller.ex
+++ lib/safira_web/controllers/referral_controller.ex
@@ -6,35 +6,30 @@ defmodule SafiraWeb.ReferralController do
   alias Safira.Contest
   alias Safira.Contest.Redeem
 
-
   action_fallback SafiraWeb.FallbackController
 
   plug Safira.Authorize, :attendee
 
   def show(conn, %{"id" => id}) do
     referral = Contest.get_referral_preload!(id)
+
     case referral.available do
       true ->
-        user = get_user(conn)
+        user = Accounts.get_user(conn)
         redeem_params = %{badge_id: referral.badge_id, attendee_id: user.attendee.id}
+
         with {:ok, %Redeem{} = _redeem} <- Contest.create_redeem(redeem_params) do
-          Contest.update_referral(referral,%{available: false, attendee_id: user.attendee.id})
+          Contest.update_referral(referral, %{available: false, attendee_id: user.attendee.id})
+
           conn
           |> put_status(:created)
           |> json(%{referral: "Referral redeemed successfully"})
         end
-      _ ->
-          conn
-          |> put_status(:unauthorized)
-          |> json(%{referral: "Referral not available"})
-    end
-  end
 
-  defp get_user(conn) do
-    with  %User{} = user <- Guardian.Plug.current_resource(conn) do
-      user
-      |> Map.fetch!(:id)
-      |> Accounts.get_user_preload!()
+      _ ->
+        conn
+        |> put_status(:unauthorized)
+        |> json(%{referral: "Referral not available"})
     end
   end
 end
