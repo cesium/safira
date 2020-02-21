c9ac221a23539a2d34a305dd7abbe947789792f1
0urobor0s <0urobor0s@users.noreply.github.com>
2020-02-21 01:46:54 +0000

    Fix winner

--- lib/mix/tasks/get.winner.ex
+++ lib/mix/tasks/get.winner.ex
@@ -5,18 +5,18 @@ defmodule Mix.Tasks.Get.Winner do
 
   def run(args) do
     cond do
-      length(args)!= 0->
-        Mix.shell.info "No arguments needed"
+      not Enum.empty?(args) ->
+        Mix.shell().info("No arguments needed")
+
       true ->
         create()
     end
   end
 
   defp create() do
-    Mix.Task.run "app.start"
-    
+    Mix.Task.run("app.start")
+
     Contest.get_winner()
-    |> Enum.map(&(IO.puts/1))
-    
+    |> Enum.map(&IO.puts/1)
   end
 end
