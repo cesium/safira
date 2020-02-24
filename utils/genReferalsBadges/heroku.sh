618709c2731461c9ac3830b6e85604c9460040ce
0urobor0s <0urobor0s@users.noreply.github.com>
2020-02-24 19:55:49 +0000

    Gen Referrals

--- utils/genReferalsBadges/heroku.sh
+++ utils/genReferalsBadges/heroku.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-heroku run POOL_SIZE=2 mix gen.referrals $1 $2 --app sei19-safira-prod > $1.in
+heroku run POOL_SIZE=2 mix gen.referrals $1 $2 --app safira20-prod > $1.in
 echo "sai do heroku"
--- utils/genReferalsBadges/script.sh
+++ utils/genReferalsBadges/script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #function geraPDF(){
  # heroku run rake sei:qrc_generator\[$1,$2,SU\]
-  tail -n +8 $1.in | sed  's///g' > temp
-  cat temp > $1.in
+#  tail -n +1 $1.in | sed  's/
+#//g' > temp
+  #cat temp > $1.in
   make $1.pdf
   mv $1.pdf pdf
 #}
