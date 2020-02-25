a34407c5dd52b09441cc97fc3a5d8b7fd8374d2d
0urobor0s <0urobor0s@users.noreply.github.com>
2020-02-25 12:11:02 +0000

    Give all attendees a badge

---
+++ lib/mix/tasks/gift.all.attendees.badge.ex
@@ -0,0 +1,32 @@
+defmodule Mix.Tasks.Gift.All.Attendees.Badge do
+  use Mix.Task
+
+  alias Safira.Accounts
+  alias Safira.Contest
+  alias Safira.Contest.Badge
+
+  def run(args) do
+    cond do
+      length(args) != 1 ->
+        Mix.shell().info("Needs to receive only an id.")
+
+      true ->
+        args |> List.first() |> String.to_integer() |> create
+    end
+  end
+
+  defp create(badge_id) do
+    Mix.Task.run("app.start")
+
+    with _badge = %Badge{} <- Contest.get_badge!(badge_id) do
+      Accounts.list_attendees()
+      |> Enum.each(fn a ->
+        Contest.create_redeem(%{
+          attendee_id: a.id,
+          manager_id: 1,
+          badge_id: badge_id
+        })
+      end)
+    end
+  end
+end
