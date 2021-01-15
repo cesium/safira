419451955540457000b8bdf9d85675020c16f572
joaop21 <jpsilva9898@gmail.com>
2021-01-15 13:02:30 +0000

    Add channel id attribute

--- lib/safira/accounts/company.ex
+++ lib/safira/accounts/company.ex
@@ -1,13 +1,14 @@
 defmodule Safira.Accounts.Company do
   use Ecto.Schema
   import Ecto.Changeset
-  
+
   alias Safira.Contest.Badge
   alias Safira.Accounts.User
 
   schema "companies" do
     field :name, :string
     field :sponsorship, :string
+    field :channel_id, :string
     belongs_to :user, User
     belongs_to :badge, Badge
 
@@ -20,5 +21,6 @@ defmodule Safira.Accounts.Company do
     |> cast(attrs, [:name, :sponsorship, :badge_id])
     |> cast_assoc(:user)
     |> validate_required([:name, :sponsorship])
+    |> unique_constraint(:channel_id)
   end
 end
