9ac48a0ee07ab609b3e40ebe7f620fe2a261c76f
joaop21 <jpsilva9898@gmail.com>
2021-01-15 14:03:52 +0000

    Add unique index

--- priv/repo/migrations/20210115125305_add_company_channel_id.exs
+++ priv/repo/migrations/20210115125305_add_company_channel_id.exs
@@ -5,5 +5,7 @@ defmodule Safira.Repo.Migrations.AddCompanyChannelId do
     alter table(:companies) do
       add :channel_id, :string
     end
+
+    create unique_index(:companies, [:channel_id])
   end
 end
