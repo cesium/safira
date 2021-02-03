f626577733e8a1b9bf10dab8ff60c8f4f96bbe9a
jpcorreia99 <jpcorreia99@gmail.com>
2021-02-03 21:29:14 +0000

    delete discord_association_update_attendee

--- lib/safira/accounts/accounts.ex
+++ lib/safira/accounts/accounts.ex
@@ -138,11 +138,6 @@ defmodule Safira.Accounts do
     |> Repo.update()
   end
 
-  def discord_association_update_attendee(%Attendee{} = attendee, attrs) do
-    attendee
-    |> Attendee.volunteer_changeset(attrs)
-    |> Repo.update()
-  end
 
   def delete_attendee(%Attendee{} = attendee) do
     Repo.delete(attendee)
