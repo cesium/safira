9ec0800a5cbfc4a89379ba28adf785a723945f47
joaop21 <jpsilva9898@gmail.com>
2021-02-05 02:03:42 +0000

    Add endpoint for daily leaderboard

--- lib/safira_web/controllers/leaderboard_controller.ex
+++ lib/safira_web/controllers/leaderboard_controller.ex
@@ -9,4 +9,19 @@ defmodule SafiraWeb.LeaderboardController do
     attendees = Contest.list_leaderboard()
     render(conn, SafiraWeb.LeaderboardView, "index.json", attendees: attendees)
   end
+
+  def daily(conn, %{"date" => date_params}) do
+    case Date.from_iso8601(date_params) do
+      {:ok, result} ->
+
+        attendees = Contest.list_daily_leaderboard(result)
+        #IO.inspect(attendees)
+        render(conn, SafiraWeb.LeaderboardView, "index.json", attendees: attendees)
+
+      {:error, _error} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Date should be in {YYYY}-{MM}-{DD} format"})
+    end
+  end
 end
