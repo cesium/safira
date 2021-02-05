9fd5774bf8a0ba024f0a8beba5c7431b17859d60
jpcorreia99 <jpcorreia99@gmail.com>
2021-02-05 16:07:31 +0000

    create gen.users_from_csv

--- lib/safira/email.ex
+++ lib/safira/email.ex
@@ -9,6 +9,7 @@ defmodule Safira.Email do
     |> from(System.get_env("FROM_EMAIL"))
     |> subject("Reset Password Instructions")
     |> text_body("Please visit #{System.get_env("FRONTEND_URL")}/reset?token=#{token} to reset your password")
+    |> Safira.Mailer.deliver_now()
   end
 
   def send_password_email(to_email, token) do
@@ -17,5 +18,6 @@ defmodule Safira.Email do
     |> from(System.get_env("FROM_EMAIL"))
     |> subject("Finish account registration")
     |> text_body("Please visit #{System.get_env("FRONTEND_URL")}/reset?token=#{token} to finish your account registration")
+    |> Safira.Mailer.deliver_now()
   end
 end
