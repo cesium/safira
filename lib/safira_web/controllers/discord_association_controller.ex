634d37b0d9a5fc4c9e96f785564d8643f0cad9d1
João Correia <46303606+jpcorreia99@users.noreply.github.com>
2021-02-08 18:58:36 +0000

    Improve discord association (#151)
    After talking to the team responsible for the discord bot, some changes were deemed necessary:
    
    - The discord_association_controller now returns the role of the user associated with the given code. Since the application only generates unique association_codes for attendees, the association codes for companies, speakers and staff must be hardcoded on the .env file and they're the same for all companies/speakers/staff.
    
    - Create a show method which, given a discord_id, returns the attendee's ID, this should be used by the bot when making request for giving extra tokens, for example.

--- lib/safira_web/controllers/discord_association_controller.ex
+++ lib/safira_web/controllers/discord_association_controller.ex
@@ -6,6 +6,19 @@ defmodule SafiraWeb.DiscordAssociationController do
 
   action_fallback SafiraWeb.FallbackController
 
+  def show(conn, %{"id" => discord_id}) do
+    cond do
+      Accounts.is_manager(conn) ->
+        show_aux(conn, discord_id)
+
+      true ->
+        conn
+        |> put_status(:unauthorized)
+        |> json(%{error: "Cannot access resource"})
+        |> halt()
+    end
+  end
+
   # association_params= %{"discord_association_code" => discord_association_code, "discord_id" => discord_id}
   def create(conn, association_params) do
     cond do
@@ -20,7 +33,43 @@ defmodule SafiraWeb.DiscordAssociationController do
     end
   end
 
-  defp association_aux(conn, %{
+  defp show_aux(conn, discord_id) do
+    attendee = Accounts.get_attendee_by_discord_id(discord_id)
+
+    cond do
+      not is_nil(attendee) ->
+        conn
+        |> put_status(:ok)
+        |> json(%{id: attendee.id})
+
+      true ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "No attendee with that discord_id"})
+    end
+  end
+
+  defp association_aux(
+         conn,
+         association_params = %{
+           "discord_association_code" => discord_association_code,
+           "discord_id" => _discord_id
+         }
+       ) do
+    # System.get_env can't be used in a match clause
+    company_code = System.get_env("COMPANY_CODE")
+    staff_code = System.get_env("STAFF_CODE")
+    speaker_code = System.get_env("SPEAKER_CODE")
+
+    case discord_association_code do
+      ^company_code -> notify_succesful_association(conn, "empresa")
+      ^staff_code -> notify_succesful_association(conn, "staff")
+      ^speaker_code -> notify_succesful_association(conn, "orador")
+      _ -> associate_attendee(conn, association_params)
+    end
+  end
+
+  defp associate_attendee(conn, %{
          "discord_association_code" => discord_association_code,
          "discord_id" => discord_id
        }) do
@@ -31,15 +80,18 @@ defmodule SafiraWeb.DiscordAssociationController do
         # no need for checking if discord_id is valid
         # since the the user's discord_id is obtained by the bot through the discord API
         Accounts.update_attendee_association(attendee, %{discord_id: discord_id})
-
-        conn
-        |> put_status(:created)
-        |> json(%{association: "Attendee discord_id successfully associated"})
+        notify_succesful_association(conn, "participante")
 
       true ->
         conn
         |> put_status(:not_found)
-        |> json(%{error: "Attendee not found"})
+        |> json(%{error: "Unable to associate"})
     end
+  end
+
+  defp notify_succesful_association(conn, role) do
+    conn
+    |> put_status(:created)
+    |> json(%{association: role})
   end
 end
