a1e9b64cab6823b358074e623f563c6d680fc5dd
Tiago Fontes <tiagorafaelfontes39@gmail.com>
2021-02-08 18:51:23 +0000

    Mix format on files

--- priv/repo/migrations/20210120134552_add_tokens_badge.exs
+++ priv/repo/migrations/20210120134552_add_tokens_badge.exs
@@ -2,9 +2,8 @@ defmodule Safira.Repo.Migrations.AddTokensBadge do
   use Ecto.Migration
 
   def change do
-
     alter table(:badges) do
       add :tokens, :integer
+    end
   end
-end
 end
--- priv/repo/migrations/20210207173118_add_token_balance_attendee.exs
+++ priv/repo/migrations/20210207173118_add_token_balance_attendee.exs
@@ -2,9 +2,8 @@ defmodule Safira.Repo.Migrations.AddTokenBalanceAttendee do
   use Ecto.Migration
 
   def change do
-
     alter table(:attendees) do
       add :token_balance, :integer
+    end
   end
-end
 end
