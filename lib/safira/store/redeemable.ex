d58ba7def6e562fb8808307b104688a4fac57b04
João Costa <jcosta998@hotmail.com>
2021-02-12 16:23:52 +0000

    update redeemable model

--- lib/safira/store/redeemable.ex
+++ lib/safira/store/redeemable.ex
@@ -7,28 +7,26 @@ defmodule Safira.Store.Redeemable do
   alias Safira.Accounts.Attendee
 
   schema "redeemables" do
-    field(:img, Safira.Avatar.Type)
-    field(:name, :string)
-    field(:description, :string)
-    field(:price, :integer)
-    field(:redeem_limit, :integer)
-    field(:num_redeems, :integer, default: 0)
-    field(:max_per_user, :integer)
+    field :img, Safira.Avatar.Type
+    field :name, :string
+    field :description, :string
+    field :price, :integer
+    field :stock, :integer
+    field :max_per_user, :integer
 
-    many_to_many(:attendees, Attendee, join_through: Buy)
+    many_to_many :attendees, Attendee, join_through: Buy 
 
     timestamps()
   end
 
   def changeset(redeemable, attrs) do
     redeemable
-    |> cast(attrs, [:name, :price, :redeem_limit, :num_redeems, :max_per_user])
+    |> cast(attrs, [:name, :price, :stock, :max_per_user])
     |> cast_attachments(attrs, [:img])
-    |> validate_required([:name, :price, :redeem_limit, :num_redeems, :max_per_user])
+    |> validate_required([:name, :price, :stock, :max_per_user])
     |> validate_length(:name, min: 1, max: 255)
     |> validate_number(:price, greater_than: 0)
-    |> validate_number(:redeem_limit, greater_than: 0)
-    |> validate_number(:num_redeems, greater_than: 0)
+    |> validate_number(:stock, greater_than: 0)
     |> validate_number(:max_per_user, greater_than: 0)
   end
 end
--- priv/repo/migrations/20210211161258_update_redeemables_table.exs
+++ priv/repo/migrations/20210211161258_update_redeemables_table.exs
@@ -3,6 +3,10 @@ defmodule Safira.Repo.Migrations.UpdateRedeemablesTable do
 
   def change do
     rename table("redeemables"), :avatar, to: :img
+    rename table("redeemables"), :redeem_limit, to: :stock
+    alter table("redeemables") do
+      remove :num_redeems
+    end
 
   end
 end
