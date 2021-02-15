5081ea661bc65de58efb4f87ad2361180e127c51
joaop21 <jpsilva9898@gmail.com>
2021-02-15 18:54:01 +0000

    Add schema and context

---
+++ lib/safira/interactions/bonus.ex
@@ -0,0 +1,22 @@
+defmodule Safira.Interactions.Bonus do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Safira.Accounts.Attendee
+  alias Safira.Accounts.Company
+
+  schema "bonuses" do
+    field :count, :integer
+
+    belongs_to :attendee, Attendee, foreign_key: :attendee_id, type: :binary_id
+    belongs_to :company, Company
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(bonus, attrs) do
+    bonus
+    |> cast(attrs, [:count, :attendee_id, :company_id])
+    |> validate_required([:count, :attendee_id, :company_id])
+  end
+end
