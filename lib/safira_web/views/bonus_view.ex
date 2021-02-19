7b176d251df4364b9e234e9e6fa29f8af1008f81
joaop21 <jpsilva9898@gmail.com>
2021-02-19 01:47:41 +0000

    Add different message

---
+++ lib/safira_web/views/bonus_view.ex
@@ -0,0 +1,16 @@
+defmodule SafiraWeb.BonusView do
+  use SafiraWeb, :view
+
+  def render("bonus.json", changes) do
+    attendee = Map.get(changes, :update_attendee)
+    bonus = Map.get(changes, :upsert_bonus)
+
+    %{
+      name: attendee.name,
+      attendee_id: attendee.id,
+      token_bonus: Application.fetch_env!(:safira, :token_bonus),
+      bonus_count: bonus.count,
+      company_id: bonus.company_id
+    }
+  end
+end
