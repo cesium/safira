de84c61ffadb0a9f44d16f9df4f57f510db8d1ae
joaop21 <jpsilva9898@gmail.com>
2021-02-20 22:05:30 +0000

    Change gen redeemables task

--- lib/mix/tasks/gen.redeemables.ex
+++ lib/mix/tasks/gen.redeemables.ex
@@ -1,25 +1,25 @@
 defmodule Mix.Tasks.Gen.Redeemables do
     use Mix.Task
-  
+
     alias NimbleCSV.RFC4180, as: CSV
-  
+
     @shortdoc "Generates the event Redeemables from a CSV"
-  
+
     @moduledoc """
     This CSV is waiting for:
       name,price,stock,max_per_user,description,path_to_image
     """
-  
+
     def run(args) do
       cond do
         Enum.empty?(args) ->
           Mix.shell().info("Needs to receive a file URL.")
-  
+
         true ->
           args |> List.first() |> create
       end
     end
-  
+
     defp create(path) do
       Mix.Task.run("app.start")
       path
@@ -27,7 +27,7 @@ defmodule Mix.Tasks.Gen.Redeemables do
       |> Enum.map(fn redeemable ->
         Safira.Store.create_redeemable(redeemable) end)
     end
-  
+
     defp parse_csv(path) do
       path
       |> File.stream!()
@@ -46,18 +46,18 @@ defmodule Mix.Tasks.Gen.Redeemables do
         }
       end)
     end
-  
+
     defp check_image_filename(image_path) do
       if String.trim(image_path) == "" do
-        "avatar-missing.png"
+        "redeemable-missing.png"
       else
         String.split(image_path, "/") |> List.last()
       end
     end
-  
+
     defp check_image_path(image_path) do
       if String.trim(image_path) == "" do
-        "#{File.cwd!()}/assets/static/images/avatar-missing.png"
+        "#{File.cwd!()}/assets/static/images/redeemable-missing.png"
       else
         image_path
       end
