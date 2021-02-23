73a853629df15415a714553395f9ad9508542ba4
Tiago Fontes <36608612+tsource98@users.noreply.github.com>
2021-02-23 22:17:50 +0000

    Export attendees and entries (#202)

---
+++ lib/mix/tasks/export.attendees.entries.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.Export.Attendees.Entries do
+  use Mix.Task
+
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    Mix.shell().info("uuid,name,email,entries")
+
+    Safira.Accounts.list_active_attendees()
+    |> Enum.each(fn a -> csv_io(a) |> Mix.shell().info end)
+  end
+
+  defp csv_io(attendee) do
+    "#{attendee.id},#{attendee.name},#{Safira.Accounts.get_user!(attendee.user_id).email},#{
+      attendee.entries
+    }"
+  end
+end
