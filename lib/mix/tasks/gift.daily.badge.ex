04719b369919eec9a7d2016402d1c8028c54049d
joaop21 <jpsilva9898@gmail.com>
2021-02-24 00:41:49 +0000

    Add task for give daily badges

---
+++ lib/mix/tasks/gift.daily.badge.ex
@@ -0,0 +1,46 @@
+defmodule Mix.Tasks.Gift.Daily.Badge do
+  use Mix.Task
+
+  import Ecto.Query, warn: false
+
+  alias Safira.Accounts.Attendee
+  alias Safira.Contest
+  alias Safira.Contest.Redeem
+  alias Safira.Contest.Badge
+
+  alias Safira.Repo
+
+  def run(args) do
+    cond do
+      length(args) != 2 ->
+        Mix.shell().info("Needs to receive badge_id and the date of the day.")
+
+      true ->
+        args |> create()
+    end
+  end
+
+  def create(args) do
+
+    {badge_id, date} = {args |> Enum.at(0), args |> Enum.at(1)}
+
+    Repo.all(
+      from a in Attendee,
+        join: r in Redeem, on: a.id == r.attendee_id,
+        join: b in Badge, on: r.badge_id == b.id,
+        where: not(is_nil a.user_id) and fragment("?::date", r.inserted_at) == ^date,
+        preload: [badges: b]
+    )
+    |> Enum.map(fn a -> Map.put(a, :badge_count, length(a.badges)) end)
+    |> Enum.filter(fn a -> a.badge_count > 0 end)
+    |> Enum.each(fn a ->
+      Contest.create_redeem(%{
+        attendee_id: a.id,
+        manager_id: 1,
+        badge_id: badge_id
+      })
+    end)
+
+  end
+
+end
