0fbda928328500b45aabb99e14833d13e998aa74
DrPeterPun <61242967+DrPeterPun@users.noreply.github.com>
2022-01-28 17:57:42 +0000

    admins should now not need a valid time to add a badge (#247)

--- lib/safira/contest/redeem.ex
+++ lib/safira/contest/redeem.ex
@@ -2,8 +2,10 @@ defmodule Safira.Contest.Redeem do
   use Ecto.Schema
   import Ecto.Changeset
   alias Safira.Contest.Badge
+  alias Safira.Contest
   alias Safira.Accounts.Attendee
   alias Safira.Accounts.Manager
+  alias Safira.Accounts
 
   schema "redeems" do
     belongs_to(:attendee, Attendee, foreign_key: :attendee_id, type: :binary_id)
@@ -14,7 +16,7 @@ defmodule Safira.Contest.Redeem do
   end
 
   @doc false
-  def changeset(redeem, attrs) do
+  def changeset(redeem, attrs, user_type \\ :manager) do
     redeem
     |> cast(attrs, [:attendee_id, :manager_id, :badge_id])
     |> validate_required([:attendee_id, :badge_id])
@@ -22,12 +24,22 @@ defmodule Safira.Contest.Redeem do
       name: :unique_attendee_badge,
       message: "An attendee can't have the same badge twice"
     )
-    |> is_within_period()
+    |> check_period(user_type)
   end
 
-  def is_within_period(changeset) do
-    {_, badge} = fetch_field(changeset, :badge)
-    curr = Datetime.utc_now()
+  defp check_period(changeset, user_type) do
+    case user_type do
+      :admin ->
+          changeset
+      _ ->
+        is_within_period(changeset)
+    end
+  end
+
+  defp is_within_period(changeset) do
+    {_, badge_id} = fetch_field(changeset, :badge_id)
+    badge = Contest.get_badge!(badge_id)
+    curr = DateTime.utc_now()
 
     cond do
       Datetime.compare(curr, badge.start_badge) == :lt ->
---
+++ priv/repo/migrations/20220121123022_add_admin_to_manager.exs
@@ -0,0 +1,10 @@
+defmodule Safira.Repo.Migrations.AddAdminToManager do
+  use Ecto.Migration
+
+  def change do
+    alter table(:managers) do
+      add :is_admin, :boolean, default: false, null: false
+    end
+
+  end
+end
