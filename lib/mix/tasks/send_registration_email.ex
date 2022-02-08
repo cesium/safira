175345a3ad6612aea5b258ebf7a4ad03bb6256f8
jtexeira <a85504@alunos.uminho.pt>
2022-02-08 19:50:08 +0000

    Add task to send reg emails

---
+++ lib/mix/tasks/send_registration_email.ex
@@ -0,0 +1,37 @@
+defmodule Mix.Tasks.Gift.All.Attendees.Badge do
+  use Mix.Task
+
+  alias Safira.Accounts
+  alias Safira.Contest
+  alias Safira.Contest.Badge
+
+  def run(args) do
+    cond do
+      length(args) != 1 ->
+        Mix.shell().info("Needs to receive only an id.")
+
+      true ->
+        args |> List.first() |> String.to_integer() |> create
+    end
+  end
+
+  defp create(badge_id) do
+    Mix.Task.run("app.start")
+
+    with _badge = %Badge{} <- Contest.get_badge!(badge_id) do
+      Accounts.list_users()
+      |> Enum.each(fn a ->
+        send_mail(a)
+      end)
+    end
+  end
+
+  defp send_mail(transaction) do
+    user = Auth.reset_password_token(user)
+
+    Safira.Email.send_registration_email(
+      user.email,
+      user.reset_password_token
+    )
+  end
+end
