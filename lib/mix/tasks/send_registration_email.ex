3c3454994916fcfd734975d3e58c80b4d70817f2
João Teixeira <38398296+jtexeira@users.noreply.github.com>
2022-02-08 20:16:02 +0000

    Add task to send reg emails (#257)
    * Add task to send reg emails
    
    * Change Task name
    
    * Fix typo
    
    * I dont know elixir
    
    * Add imports

---
+++ lib/mix/tasks/send_registration_email.ex
@@ -0,0 +1,24 @@
+defmodule Mix.Tasks.Send.RegistrationEmails do
+  use Mix.Task
+
+  alias Safira.Accounts
+  alias Safira.Auth
+
+  def run(args) do
+    Mix.Task.run("app.start")
+
+    Accounts.list_users()
+    |> Enum.each(fn a ->
+      send_mail(a)
+    end)
+  end
+
+  defp send_mail(user) do
+    user = Auth.reset_password_token(user)
+
+    Safira.Email.send_registration_email(
+      user.email,
+      user.reset_password_token
+    )
+  end
+end
