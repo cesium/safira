1e4939efb13199d412a9f4a9db07a3890521f510
DrPeterPun <pedro_pereira@outlook.pt>
2022-02-09 21:56:03 +0000

    added db migration file

---
+++ priv/repo/migrations/20220209215400_add_prize_quantity.exs
@@ -0,0 +1,10 @@
+defmodule Safira.Repo.Migrations.AddRedeemedQuantity do
+  use Ecto.Migration
+
+  def change do
+    alter table(:attendee_prizes) do
+      add :redeemed, :integer, null: false, default: 0
+    end
+
+  end
+end
