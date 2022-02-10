3e8ac979ad73d9985f1a167981e6b5f1bf929368
DrPeterPun <pedro_pereira@outlook.pt>
2022-02-10 00:10:29 +0000

    s

--- priv/repo/migrations/20220209215400_add_prize_quantity.exs
+++ priv/repo/migrations/20220209215400_add_prize_quantity.exs
@@ -2,7 +2,7 @@ defmodule Safira.Repo.Migrations.AddPrizeQuantity do
   use Ecto.Migration
 
   def change do
-    alter table(:attendee_prizes) do
+    alter table(:attendees_prizes) do
       add :redeemed, :integer, null: false, default: 0
     end
 
