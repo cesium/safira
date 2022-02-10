2b63913407b0e3349fc7d2cbf3536418b8424cae
Matilde Bravo <matildeopbravo@gmail.com>
2022-02-10 14:46:16 +0000

    Added verification to prevent badges for simultaneous activities to be gifted to the same attendee (#252)
    * Added fields to badges.csv to keep track of badge-gifting period, apart from the period of the activity itself. Made corresponding changes to parsing and badge structure
    
    * Added migration for add and begin end badges
    
    * Updated badges.csv to reflect te changes made on badge format
    
    * Added verification before gifting badge, to check whether the attendee already has a badge of an activity for that time and date
    
    * Changed verification to allow simultaneous badges for coincidental activities which aren't workshops, pitches or talks
    
    * Remove whitespace
    
    * Fixing the typos that find a way to come back on every PR
    
    * Fix datetime typo
    
    * Fix datetime typo
    
    * Typo
    
    * Fix datetime compare being reversed
    
    * Fix fetching data on incompatible badges
    
    Co-authored-by: João Teixeira <a85504@alunos.uminho.pt>

--- lib/safira/contest/redeem.ex
+++ lib/safira/contest/redeem.ex
@@ -26,7 +26,39 @@ defmodule Safira.Contest.Redeem do
       name: :unique_attendee_badge,
       message: "An attendee can't have the same badge twice"
     )
-    |> check_period(user_type)
+    |> is_within_period()
+    |> simultaneous_constraint()
+  end
+
+  # verificar se não tem já um badge de uma talk ou workshop nessa hora
+  def simultaneous_constraint(changeset) do
+    {_, attendee_id} = fetch_field(changeset, :attendee_id)
+    {_, badge_id} = fetch_field(changeset, :badge_id)
+    attendee = Accounts.get_attendee!(attendee_id)
+    badges = attendee.badges
+    badge = Contest.get_badge!(badge_id)
+
+    if Enum.any?(badges, &coincidental(badge, &1)) do
+      add_error(
+        changeset,
+        :badge,
+        "Attendee already has badge for an activity within that period"
+      )
+    else
+      changeset
+    end
+  end
+
+  # workshops, pitches, talks
+  def is_unique_type(type) do
+    type == 6 or type == 7 or type == 9
+  end
+
+  # verifica se os badges sao simultaneos e algum deles é um workshop
+  def coincidental(badge_a, badge_b) do
+    DateTime.compare(badge_a.begin, badge_b.end) == :lt and
+      DateTime.compare(badge_b.begin, badge_a.end) == :lt and
+      is_unique_type(badge_a.type) and is_unique_type(badge_b.type)
   end
 
   defp check_period(changeset, user_type) do
