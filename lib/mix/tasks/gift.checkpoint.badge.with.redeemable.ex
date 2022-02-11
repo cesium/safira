6422ec64d935cbedc324ee4182c7c43dce65d527
DrPeterPun <pedro_pereira@outlook.pt>
2022-02-11 18:00:28 +0000

    hopefuly os commit ta bem feito

---
+++ lib/mix/tasks/gift.checkpoint.badge.with.redeemable.ex
@@ -0,0 +1,131 @@
+defmodule Mix.Tasks.Gift.Company.Checkpoint.Badge.With.Redeemable do
+  use Mix.Task
+
+  import Ecto.Query, warn: false
+
+  alias Safira.Accounts.Attendee
+  alias Safira.Contest.Badge
+  alias Safira.Contest.Redeem
+
+  alias Safira.Repo
+  alias Ecto.Multi
+  alias Ecto.Multi
+
+  @shortdoc "Gives checkpoint badge to attendees that reach that checkpoint"
+
+  @moduledoc """
+    This task needs to receive:
+      - badge_id: Badge's ID to give
+      - badge_count: Number of Badges thar an attendee must have
+          to receive the checkpoint basge
+      - entries: Number of entries that an attendee receives for
+          having this badge
+      - badge_type: type of badge to confirm
+      - redeemable_id: Redeemable's ID that is associated with the given badge
+  """
+
+  def run(args) do
+    cond do
+      length(args) != 5 ->
+        Mix.shell().info("Needs to receive badge_id, badge_count, entries ,badge_type and redeemable_id.")
+
+      true ->
+        create(args)
+    end
+  end
+
+  defp create(args) do
+    Mix.Task.run("app.start")
+
+    args
+    |> validate_args()
+    |> map_args()
+    |> gift_redeemable()
+    |> gift_badge()
+
+  end
+
+  defp validate_args(args) do
+    try do
+      args
+    |> Enum.map(fn x -> Integer.parse(x) |> elem(0) end)
+    rescue
+      ArgumentError ->
+        Mix.shell().info("All arguments should be integers")
+    end
+  end
+
+  defp map_args(args) do
+    %{
+      badge_id: Enum.at(args, 0),
+      badge_count: Enum.at(args, 1),
+      entries: Enum.at(args, 2),
+      badge_type: Enum.at(args, 3),
+      redeemable_id: Enum.at(args, 4)
+    }
+  end
+
+  defp gift_badge(args) do
+    case Repo.get(Badge, Map.get(args, :badge_id)) do
+      %Badge{} = badge ->
+        args = Map.put(args, :badge, badge)
+        attendees = get_attendees_company_badges(args)
+        give_checkpoint_badge(args, attendees)
+
+      nil ->
+        Mix.shell().error("Badge_id needs to be valid.")
+    end
+  end
+
+  defp get_attendees_company_badges(args) do
+    Repo.all(
+      from a in Attendee,
+      join: r in Redeem, on: a.id == r.attendee_id,
+      join: b in Badge, on: r.badge_id == b.id,
+      where: b.type == ^Map.get(args, :badge_type),
+      preload: [badges: b]
+    )
+    |> Enum.map(fn a -> Map.put(a, :badge_count, length(a.badges)) end)
+    |> Enum.filter(fn x -> x.badge_count >= Map.get(args, :badge_count) end)
+  end
+
+  defp give_checkpoint_badge(args, attendees) do
+    attendees
+    |> Enum.each(fn a ->
+      %{
+        attendee_id: a.id,
+        badge_id: Map.get(args, :badge_id),
+        manager_id: 1
+      }
+      |> create_redeem(args, a, Map.get(args, :badge))
+    end)
+  end
+
+  defp create_redeem(redeem_attrs, args, attendee, badge) do
+    Multi.new()
+    |> Multi.insert(:redeem, Redeem.changeset(%Redeem{}, redeem_attrs))
+    |> Multi.update(:attendee,
+      Attendee.update_on_redeem_changeset(
+        attendee,
+        %{
+          token_balance: attendee.token_balance + badge.tokens,
+          entries: attendee.entries + Map.get(args, :entries)
+        }
+      )
+    )
+    |> Repo.transaction()
+  end
+
+  defp gift_redeemable(args) do
+    st = args
+    |> Map.get(args, :redeemable_id)
+    |> Store.buy_redeemable()
+    case st do
+      {:ok, _} ->
+        args
+      _ ->
+        {:error, "Cnat gift redeemable"}
+    end
+
+  end
+end
