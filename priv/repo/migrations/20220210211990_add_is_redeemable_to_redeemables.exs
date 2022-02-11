dd3a49c699297deef234af491d9e8644776ad86d
DrPeterPun <61242967+DrPeterPun@users.noreply.github.com>
2022-02-11 00:06:04 +0000

    Adding is_redeemable to the store (#269)
    * Add migration
    
    * Fix csv
    
    * Adapt gen.redeems
    
    * added is_redeemable to schema, to changeset and altered store.list_stoter_redeemable to only show the redeemable ones
    
    * renamed migration
    
    * renamed migration now good this time
    
    Co-authored-by: jtexeira <a85504@alunos.uminho.pt>

---
+++ priv/repo/migrations/20220210211990_add_is_redeemable_to_redeemables.exs
@@ -0,0 +1,10 @@
+defmodule Safira.Repo.Migrations.AddIsRedeemableToRedeemables do
+  use Ecto.Migration
+
+  def change do
+    alter table(:redeemables) do
+      add :is_redeemable, :boolean, default: true, null: false
+    end
+
+  end
+end
