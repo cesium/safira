24249bffc29efe0a31b7213c124aa561184c7faa
Rui Oliveira <rpoliveira02@gmail.com>
2022-09-10 21:34:06 +0000

    Refactor attendee controller tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,10 +2,7 @@ defmodule Safira.Factory do
   use ExMachina.Ecto, repo: Safira.Repo
   use Safira.UserStrategy
   use Safira.AccountFactory
-  # use Safira.UserFactory
-  # use Safira.AttendeeFactory
-  use Safira.BadgeFactory
-  use Safira.RedeemFactory
+  use Safira.ContestFactory
   use Safira.PrizeStrategy
   use Safira.PrizeFactory
 end
