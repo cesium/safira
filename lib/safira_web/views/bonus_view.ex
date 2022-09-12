e5b00d01f3067103c4ca1bd96ed1fcbf82ab7208
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-09-12 19:12:12 +0100

    Create bonus controller tests

--- lib/safira_web/views/bonus_view.ex
+++ lib/safira_web/views/bonus_view.ex
@@ -5,12 +5,16 @@ defmodule SafiraWeb.BonusView do
     attendee = Map.get(changes, :update_attendee)
     bonus = Map.get(changes, :upsert_bonus)
 
-    %{
+    value = %{
       name: attendee.name,
       attendee_id: attendee.id,
       token_bonus: Application.fetch_env!(:safira, :token_bonus),
       bonus_count: bonus.count,
       company_id: bonus.company_id
     }
+
+    IO.inspect(value)
+
+    value
   end
 end
---
+++ test/safira_web/controllers/bonus_controller_test.exs
@@ -0,0 +1,32 @@
+defmodule SafiraWeb.BonusControllerTest do
+  use SafiraWeb.ConnCase
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "give bonus" do
+    test "with valid token" do
+      company_user = create_user_strategy(:user)
+      attendee_user = create_user_strategy(:user)
+
+      company = insert(:company, user: company_user)
+      attendee = insert(:attendee, user: attendee_user)
+
+      %{conn: conn, user: _} = api_authenticate(company_user)
+
+      conn =
+        conn
+        |> post(Routes.bonus_path(conn, :give_bonus, attendee.id))
+        |> doc()
+
+      assert json_response(conn, 200)["data"] == %{
+        "name" => attendee.name,
+        "attendee_id" => attendee.id,
+        "token_bonus" => Application.fetch_env!(:safira, :token_bonus),
+        "bonus_count" => conn.assigns.get_bonus.count,
+        "company_id" => company.id
+      }
+    end
+  end
+end
