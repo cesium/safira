5061b797ae5b9d0ef7d448df10b17fc9bacb1b58
Rui Oliveira <rpoliveira02@gmail.com>
2022-09-14 10:23:08 +0100

    Create test file

---
+++ test/safira_web/controllers/deliver_prize_controller_test.exs
@@ -0,0 +1,80 @@
+defmodule SafiraWeb.DeliverPrizeControllerTest do
+  use SafiraWeb.ConnCase
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "index" do
+    test "with valid token"  do
+      badge = insert(:badge)
+      %{conn: conn, user: _} = api_authenticate(create_user_strategy(:user))
+      conn = conn
+        |> get(Routes.badge_path(conn, :index))
+        |> doc()
+      assert json_response(conn, 200)["data"] == [%{
+        "avatar" => "/images/badge-missing.png",
+        "begin" => badge.begin,
+        "description" => badge.description,
+        "end" => badge.end,
+        "id" => badge.id,
+        "name" => badge.name,
+        "tokens" => badge.tokens,
+        "type" => badge.type
+      }]
+    end
+
+    test "with invalid token", %{conn: conn} do
+      conn = conn
+        |> put_req_header("authorization", "Bearer #{"invalid"}")
+        |> get(Routes.badge_path(conn, :index))
+        |> doc()
+      assert json_response(conn, 401)["error"] == "invalid_token"
+    end
+
+    test "with no token", %{conn: conn} do
+      conn = conn
+        |> get(Routes.badge_path(conn, :index))
+        |> doc()
+      assert json_response(conn, 401)["error"] == "unauthenticated"
+    end
+  end
+
+  describe "show" do
+    test "with valid token"  do
+      badge = insert(:badge)
+      %{conn: conn, user: _} = api_authenticate(create_user_strategy(:user))
+      conn = conn
+        |> get(Routes.badge_path(conn, :show, badge.id))
+        |> doc()
+      assert json_response(conn, 200)["data"] == %{
+        "avatar" => "/images/badge-missing.png",
+        "begin" => badge.begin,
+        "description" => badge.description,
+        "end" => badge.end,
+        "id" => badge.id,
+        "name" => badge.name,
+        "tokens" => badge.tokens,
+        "type" => badge.type,
+        "attendees" => []
+      }
+    end
+
+    test "with invalid token", %{conn: conn} do
+      badge = insert(:badge)
+      conn = conn
+        |> put_req_header("authorization", "Bearer #{"invalid"}")
+        |> get(Routes.badge_path(conn, :show, badge.id))
+        |> doc()
+      assert json_response(conn, 401)["error"] == "invalid_token"
+    end
+
+    test "with no token", %{conn: conn} do
+      badge = insert(:badge)
+      conn = conn
+        |> get(Routes.badge_path(conn, :show, badge.id))
+        |> doc()
+      assert json_response(conn, 401)["error"] == "unauthenticated"
+    end
+  end
+end
