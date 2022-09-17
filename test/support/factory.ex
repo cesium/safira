773cee3980e36c058545e1cacfc97718912ef44e
Rui Lopes <rui.lilokas2003@hotmail.com>
2022-09-17 17:52:05 +0100

    Create prize controller tests (#291)

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,8 +1,9 @@
 defmodule Safira.Factory do
   use ExMachina.Ecto, repo: Safira.Repo
-  use Safira.UserStrategy
+
   use Safira.AccountsFactory
+  use Safira.UserStrategy
   use Safira.ContestFactory
   use Safira.PrizeStrategy
-  use Safira.PrizeFactory
+  use Safira.RouletteFactory
 end
