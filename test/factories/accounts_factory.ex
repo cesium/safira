ed12aefc04b24abc169f53687c2616d99494d7b1
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2022-09-22 11:16:38 +0100

    Redeem controller tests (#299)
    * Redeem controller tests
    
    * Remove broken test

--- test/factories/accounts_factory.ex
+++ test/factories/accounts_factory.ex
@@ -23,7 +23,8 @@ defmodule Safira.AccountsFactory do
           name: name,
           sponsorship: Enum.random(["exclusive", "gold", "silver", "bronze"]),
           channel_id: String.downcase(name),
-          user: build(:user)
+          user: build(:user),
+          badge: build(:badge)
         }
       end
 
