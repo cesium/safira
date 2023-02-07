a9d01756e1bca515c579f4c0c76281de1a6c191d
Rui Oliveira <rpoliveira02@gmail.com>
2023-02-07 15:16:38 +0000

    Fix email adapter attempt #6

--- config/config.exs
+++ config/config.exs
@@ -78,7 +78,7 @@ config :safira, Safira.Mailer,
   adapter: Bamboo.MailgunAdapter,
   api_key: {:system, "MAILGUN_API_KEY"},
   domain: {:system, "MAILGUN_DOMAIN"},
-  base_url: {:system, "MAILGUN_BASE_URL"}
+  base_url: {:system, "MAILGUN_BASE_URL"},
   hackney_opts: [
     recv_timeout: :timer.minutes(1)
   ]
