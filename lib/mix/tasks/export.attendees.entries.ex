a767f92fba03c592af9e50aebe82cf062b8e5f32
Rui Oliveira <rpoliveira02@gmail.com>
2023-02-17 11:47:39 +0000

    Update task for entries for final draw

--- lib/mix/tasks/export.attendees.entries.ex
+++ lib/mix/tasks/export.attendees.entries.ex
@@ -1,19 +1,74 @@
 defmodule Mix.Tasks.Export.Attendees.Entries do
   @moduledoc """
-  Task to export the entries for the final draw
+  Task to export the entries for the final draw to a CSV.
+
+  Run it as mix export.attendees.entries data/entries.csv Aggregate
+  to have one row per attendee (with the number of entries in it)
+  Run it as mix export.attendees.entries data/entries.csv Separate to have
+  one row per entry (this is needed to run the draw in random.org, for example)
+
+  As this script writes to a file, you need to run it in your local machine pointing
+  to the production database.
   """
   use Mix.Task
+  alias Safira.Accounts
 
-  def run(_) do
+  def run(args) do
     Mix.Task.run("app.start")
 
-    Mix.shell().info("uuid,name,email,entries")
+    cond do
+      length(args) != 2 ->
+        Mix.shell().info(
+          "Needs to receive a path to write the file to and whether to aggregate the entries"
+        )
 
-    Safira.Accounts.list_active_attendees()
-    |> Enum.each(fn a -> csv_io(a) |> Mix.shell().info end)
+      args |> List.last() == "Aggregate" ->
+        write_csv(List.first(args), :aggregate)
+
+      args |> List.last() == "Separate" ->
+        write_csv(List.first(args), :separate)
+
+      true ->
+        Mix.shell().info("Second argument must be equal to \"Aggregate\" and \"Separate\"")
+    end
   end
 
-  defp csv_io(attendee) do
-    "#{attendee.id},#{attendee.name},#{Safira.Accounts.get_user!(attendee.user_id).email},#{attendee.entries}"
+  defp write_csv(file, mode) do
+    data =
+      Accounts.list_active_attendees()
+      |> Enum.map(fn at -> csv_io(at, mode) end)
+      |> List.flatten()
+      |> Enum.with_index()
+      |> Enum.map(fn {entry, index} -> "#{index + 1},#{entry}" end)
+      |> add_header(mode)
+      |> Enum.intersperse("\n")
+      |> List.flatten()
+
+    File.write!(file, data)
+  end
+
+  defp csv_io(attendee, :separate) do
+    email = Accounts.get_user!(attendee.user_id).email
+
+    Enum.to_list(1..attendee.entries)
+    |> Enum.map(fn _x -> "#{attendee.id},#{attendee.name},#{attendee.nickname},#{email}" end)
+  end
+
+  defp csv_io(attendee, :aggregate) do
+    if attendee.entries > 0 do
+      [
+        "#{attendee.id},#{attendee.name},#{Accounts.get_user!(attendee.user_id).email},#{attendee.entries}"
+      ]
+    else
+      []
+    end
+  end
+
+  defp add_header(list, :aggregate) do
+    ["id,uuid,name,nickname,email,entries" | list]
+  end
+
+  defp add_header(list, :separate) do
+    ["id,uuid,name,nickname,email" | list]
   end
 end
