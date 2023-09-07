191922d9411563daa52fa82159ac92da4429bbc3
Rui Oliveira <rpoliveira02@gmail.com>
2023-09-07 10:23:11 +0100

    build: add seeds for intermediate schemas

--- priv/repo/seeds/contest.exs
+++ priv/repo/seeds/contest.exs
@@ -1,12 +1,43 @@
 defmodule Safira.Repo.Seeds.Contest do
   @moduledoc false
 
+  alias Safira.Accounts.Attendee
+  alias Safira.Contest
+  alias Safira.Contest.Badge
+  alias Safira.Repo
+
+  @redeem_probability 0.05
+
   def run do
     seed_badges()
+    seed_redeems()
   end
 
   defp seed_badges do
     Mix.Tasks.Gen.Badges.run(["data/badges.csv"])
+  end
+
+  defp seed_redeems do
+    attendees = Repo.all(Attendee)
+    badges = Repo.all(Badge)
+
+    attendee_count = Enum.count(attendees)
+    badge_count = Enum.count(badges)
+    number_redeems = round(attendee_count * badge_count * @redeem_probability)
+
+    redeems = for at <- attendees, b <- badges, do: %{
+      attendee_id: at.id,
+      badge_id: b.id
+    }
+
+    redeems
+    |> Enum.take_random(number_redeems)
+    |> Enum.each(&insert_redeem/1)
+  end
+
+  defp insert_redeem(redeem) do
+    redeem
+    |> Contest.create_redeem(:admin)
   end
 end
 
--- priv/repo/seeds/roulette.exs
+++ priv/repo/seeds/roulette.exs
@@ -1,12 +1,33 @@
 defmodule Safira.Repo.Seeds.Roulette do
   @moduledoc false
 
+  alias Safira.Accounts.Attendee
+  alias Safira.Repo
+  alias Safira.Roulette
+
+  @number_spins 40
+
   def run do
     seed_prizes()
+    seed_spins()
   end
 
   defp seed_prizes do
     Mix.Tasks.Gen.Prizes.run(["data/wheel.csv"])
+  end
+
+  defp seed_spins do
+    attendees = Repo.all(Attendee)
+    for _ <- 0..@number_spins do
+      at = Enum.random(attendees)
+
+      # Making sure attendee has enough tokens to spin the wheel
+      at = at
+      |> Attendee.update_token_balance_changeset(%{token_balance: at.token_balance + Application.fetch_env!(:safira, :roulette_cost)})
+      |> Repo.update!()
+
+      Roulette.spin_transaction(at)
+    end
   end
 end
 
