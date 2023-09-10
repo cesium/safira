1853cfccafbfc99d63449f5c2195a5de1273bb67
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2023-09-10 10:26:00 +0100

    build: Add seeds (#342)
    * build: add account seeds
    
    * build: add remaining seeds
    
    * build: add seeds for intermediate schemas

--- mix.exs
+++ mix.exs
@@ -89,7 +89,8 @@ defmodule Safira.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
       lint: ["credo --strict --all"]
---
+++ priv/repo/seeds/store.exs
@@ -0,0 +1,51 @@
+defmodule Safira.Repo.Seeds.Store do
+  @moduledoc false
+
+  alias Safira.Accounts.Attendee
+  alias Safira.Store
+  alias Safira.Store.Redeemable
+  alias Safira.Repo
+
+  @buys_amount 30
+
+  def run do
+    seed_redeemables()
+    seed_buys()
+  end
+
+  defp seed_redeemables do
+    Mix.Tasks.Gen.Redeemables.run(["data/redeemables.csv"])
+  end
+
+  defp seed_buys do
+    attendees = Repo.all(Attendee)
+    redeemables = Repo.all(Redeemable)
+
+    for i <- 0..@buys_amount, do: buy_random(redeemables, attendees)
+  end
+
+  defp buy_random(redeemables, attendees) do
+      # By making this random there is the possibility this fails if the same
+      # product is picked so much it runs out of stock. However this is not
+      # a problem, as long as the script continues running, we will just
+      # have fewer purchases
+      at = Enum.random(attendees)
+      r  = Enum.random(redeemables)
+
+      amount = Enum.random(1..r.max_per_user)
+      redeemed = Enum.random([true, false])
+
+      # Making sure attendee has enough tokens to purchase
+      at
+      |> Attendee.update_token_balance_changeset(%{token_balance: at.token_balance + amount * r.price})
+      |> Repo.update!()
+
+      for j <- 0..amount, do: Store.buy_redeemable(r.id, at)
+
+      if redeemed do
+        Store.redeem_redeemable(r.id, at, amount)
+      end
+  end
+end
+
+Safira.Repo.Seeds.Store.run()
