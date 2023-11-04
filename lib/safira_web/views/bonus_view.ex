ca219758d72a90e9c8c8fdd83018888bc404d706
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-11-04 23:23:51 +0000

    fix: change application fetch envs to application compile envs

--- lib/safira_web/views/bonus_view.ex
+++ lib/safira_web/views/bonus_view.ex
@@ -1,6 +1,8 @@
 defmodule SafiraWeb.BonusView do
   use SafiraWeb, :view
 
+  @token_bonus Application.compile_env!(:safira, :token_bonus)
+
   def render("bonus.json", changes) do
     attendee = Map.get(changes, :update_attendee)
     bonus = Map.get(changes, :upsert_bonus)
@@ -8,7 +10,7 @@ defmodule SafiraWeb.BonusView do
     %{
       name: attendee.name,
       attendee_id: attendee.id,
-      token_bonus: Application.fetch_env!(:safira, :token_bonus),
+      token_bonus: @token_bonus,
       bonus_count: bonus.count,
       company_id: bonus.company_id
     }
