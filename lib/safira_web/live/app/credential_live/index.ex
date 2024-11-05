75b5787c36b80270b76db0ed51df38f55d74fa75
João Lobo <joao@lobo.is>
2024-11-05 02:26:44 +0000

    fix: add current page

--- lib/safira_web/live/app/credential_live/index.ex
+++ lib/safira_web/live/app/credential_live/index.ex
@@ -7,6 +7,7 @@ defmodule SafiraWeb.App.CredentialLive.Index do
   def mount(_params, _session, socket) do
     {:ok,
      socket
+     |> assign(:current_page, :credential)
      |> assign(
        :credential,
        Accounts.get_credential_of_attendee!(socket.assigns.current_user.attendee)
