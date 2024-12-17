49cf09643438bc34256835c99a3937bdf392e722
Rui Oliveira <rpoliveira02@gmail.com>
2024-12-17 12:55:37 +0000

    fix: non null drops

--- priv/repo/migrations/20241217113451_add_wheel_spins.exs
+++ priv/repo/migrations/20241217113451_add_wheel_spins.exs
@@ -4,8 +4,12 @@ defmodule Safira.Repo.Migrations.AddWheelSpins do
   def change do
     create table(:wheel_spins, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :attendee_id, references(:attendees, type: :binary_id, on_delete: :delete_all)
-      add :drop_id, references(:wheel_drops, type: :binary_id, on_delete: :delete_all)
+
+      add :attendee_id, references(:attendees, type: :binary_id, on_delete: :delete_all),
+        null: false
+
+      add :drop_id, references(:wheel_drops, type: :binary_id, on_delete: :delete_all),
+        null: false
 
       timestamps(type: :utc_datetime)
     end
