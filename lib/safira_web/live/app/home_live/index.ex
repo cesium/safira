c5552dde815c6e697ea7e5fca619faf501050782
Darguima <dsgdevbraga@gmail.com>
2024-12-30 23:54:28 +0000

    formatter

--- lib/safira_web/live/app/home_live/index.ex
+++ lib/safira_web/live/app/home_live/index.ex
@@ -7,18 +7,20 @@ defmodule SafiraWeb.App.HomeLive.Index do
     companies_visited = 14
     max_level = 4
     user_level = min(div(companies_visited, 5), max_level)
-    companies_to_next_level = if user_level == max_level, do: 0, else: 5 - rem(companies_visited, 5)
+
+    companies_to_next_level =
+      if user_level == max_level, do: 0, else: 5 - rem(companies_visited, 5)
 
     prizes_by_next_level = [10, 20, 40, 100]
     next_prize = Enum.at(prizes_by_next_level, user_level)
 
     {:ok,
      assign(socket,
-      companies_visited: companies_visited,
-      user_level: user_level,
-      companies_to_next_level: companies_to_next_level,
-      next_prize: next_prize,
-      max_level: max_level
+       companies_visited: companies_visited,
+       user_level: user_level,
+       companies_to_next_level: companies_to_next_level,
+       next_prize: next_prize,
+       max_level: max_level
      )}
   end
 
