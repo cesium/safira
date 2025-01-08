4eabc947f348d9befd6df1cf360dda9e3f63cfe0
Nuno Miguel <nmpf.2005@gmail.com>
2025-01-08 01:26:37 +0000

    feat: add analytics

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,9 @@ import Config
 
 config :safira,
   ecto_repos: [Safira.Repo],
-  generators: [timestamp_type: :utc_datetime]
+  generators: [timestamp_type: :utc_datetime],
+  umami_script_url: System.get_env("UMAMI_SCRIPT_URL") || "",
+  umami_website_id: System.get_env("UMAMI_WEBSITE_ID") || ""
 
 # Flop configuration
 config :flop,
