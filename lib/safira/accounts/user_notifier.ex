087bf4f6a8a35fb86156ff4a10b6358e7bb9725c
Nuno Miguel <nmpf.2005@gmail.com>
2025-01-18 18:15:22 +0000

    feat: add welcome email to email confirmation logic

--- lib/safira/accounts/user_notifier.ex
+++ lib/safira/accounts/user_notifier.ex
@@ -8,10 +8,13 @@ defmodule Safira.Accounts.UserNotifier do
 
   use Phoenix.Swoosh, view: SafiraWeb.EmailView
 
-  defp base_email(opts) do
+  defp base_html_email(recipient, subject) do
+    sender = {Mailer.get_sender_name(), Mailer.get_sender_address()}
+
     new()
-    |> to(opts[:to])
-    |> from({Mailer.get_sender_name(), Mailer.get_sender_address()})
+    |> to(recipient)
+    |> from(sender)
+    |> subject("[#{elem(sender, 0)}] #{subject}")
   end
 
   # Delivers the email using the application mailer.
@@ -35,8 +38,7 @@ defmodule Safira.Accounts.UserNotifier do
   """
   def deliver_confirmation_instructions(user, url) do
     email =
-      base_email(to: user.email)
-      |> subject("SEI | Confirm your email")
+      base_html_email(user.email, "Confirm your email")
       |> assign(:user_name, user.name)
       |> assign(:confirm_email_link, url)
       |> render_body("confirm_email.html")
@@ -52,8 +54,7 @@ defmodule Safira.Accounts.UserNotifier do
   """
   def deliver_reset_password_instructions(user, url) do
     email =
-      base_email(to: user.email)
-      |> subject("SEI | Reset password")
+      base_html_email(user.email, "Reset your password")
       |> assign(:user_name, user.name)
       |> assign(:reset_password_link, url)
       |> render_body("reset_password.html")
@@ -69,8 +70,7 @@ defmodule Safira.Accounts.UserNotifier do
   """
   def deliver_welcome_email(user) do
     email =
-      base_email(to: user.email)
-      |> subject("Welcome to SEI")
+      base_html_email(user.email, "Welcome to SEI")
       |> assign(:user_name, user.name)
       |> render_body("welcome.html")
 
