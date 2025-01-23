9fad019ec5aaae00086b395bcda42f67d338e71b
Darguima <dsgdevbraga@gmail.com>
2025-01-23 15:57:38 +0000

    fix test

--- test/safira/accounts_test.exs
+++ test/safira/accounts_test.exs
@@ -217,7 +217,7 @@ defmodule Safira.AccountsTest do
       {:error, changeset} =
         Accounts.apply_user_email(user, "invalid", %{email: unique_user_email()})
 
-      assert %{current_password: ["is not valid"]} = errors_on(changeset)
+      assert %{current_password: ["password not correct"]} = errors_on(changeset)
     end
 
     test "applies the email without persisting it", %{user: user} do
@@ -339,7 +339,7 @@ defmodule Safira.AccountsTest do
       {:error, changeset} =
         Accounts.update_user_password(user, "invalid", %{password: valid_user_password()})
 
-      assert %{current_password: ["is not valid"]} = errors_on(changeset)
+      assert %{current_password: ["password not correct"]} = errors_on(changeset)
     end
 
     test "updates the password", %{user: user} do
