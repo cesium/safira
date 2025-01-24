7c19d15afd97eee21953e9dedb9a95b7ccb804ae
AfonsoMartins26 <afonsopm2005@gmail.com>
2025-01-24 17:40:31 +0000

    feat: remove gap

--- lib/safira_web/live/landing/team_live/components/team.ex
+++ lib/safira_web/live/landing/team_live/components/team.ex
@@ -11,12 +11,12 @@ defmodule SafiraWeb.Teamcomponent do
     ~H"""
     <div class="text-left my-8">
       <h2 class="text-2xl font-bold uppercase text-white mb-6"><%= @team_name %></h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         <div :for={member <- @members} class="flex flex-col items-center">
           <img
             src={Uploaders.Member.url({member.image, member}, :original, signed: true)}
             alt={"#{member.name}'s photo"}
-            class="w-36 h-36 object-cover"
+            class="w-48 h-48 object-cover"
           />
           <p class="mt-2 text-lg font-semibold text-white uppercase"><%= member.name %></p>
         </div>
