2b4c354bb88a0c76808441b0d6579b7dbb4d7940
AfonsoMartins26 <afonsopm2005@gmail.com>
2025-01-30 20:58:06 +0000

    feat: request change and fix name folder

--- lib/safira_web/live/landing/team_live/index.ex
+++ lib/safira_web/live/landing/team_live/index.ex
@@ -4,6 +4,8 @@ defmodule SafiraWeb.Landing.TeamLive.Index do
   alias Safira.Teams
   import SafiraWeb.Teamcomponent
 
+  on_mount {SafiraWeb.VerifyFeatureFlag, "team_enabled"}
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, teams: Teams.list_teams(preloads: [:team_members]))}
