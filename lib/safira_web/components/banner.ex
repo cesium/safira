ac0f8593eb155d2205fd87a13eaf87cc303c520a
Nuno Miguel <nmpf.2005@gmail.com>
2025-02-11 14:14:37 +0000

    feat: improve spotlight and file uploader

--- lib/safira_web/components/banner.ex
+++ lib/safira_web/components/banner.ex
@@ -9,7 +9,7 @@ defmodule SafiraWeb.Components.Banner do
     <div
       id="banner"
       phx-hook="Banner"
-      class="relative w-full bg-white text-center p-4 text-black font-bold shadow-lg z-10 transition-transform transform"
+      class="relative w-full bg-gradient-to-b from-accent to-transparent from-85% text-center pt-2 pb-3 text-primary/70 font-bold z-10 transition-transform transform"
       data-end={@end_time}
     >
       <p><%= @text %></p>
