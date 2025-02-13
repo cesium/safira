8524a2f9e98d0049f8d2d2bda2c95eece2749706
Nuno Miguel <30806591+nunom27@users.noreply.github.com>
2025-02-13 16:04:26 +0000

    feat: add remove attendee tokens by day (#533)

--- lib/safira/contest.ex
+++ lib/safira/contest.ex
@@ -403,9 +403,10 @@ defmodule Safira.Contest do
       })
     end)
     |> Multi.merge(fn %{badge_redeem: badge_redeem} ->
-      change_attendee_tokens_transaction(
+      remove_attendee_tokens_by_day_transaction(
         badge_redeem.attendee,
-        max(badge_redeem.attendee.tokens - badge_redeem.badge.tokens, 0)
+        badge_redeem.badge.tokens,
+        badge_redeem.inserted_at
       )
     end)
     |> Repo.transaction()
@@ -843,6 +844,43 @@ defmodule Safira.Contest do
     end)
     |> Multi.insert_or_update(daily_tokens_update_operation_name, fn changes ->
       DailyTokens.changeset(Map.get(changes, daily_tokens_fetch_operation_name), %{tokens: tokens})
+    end)
+  end
+
+  @doc """
+  Transaction for removing tokens from the attendee and the daily tokens by day.
+  """
+  def remove_attendee_tokens_by_day_transaction(
+        attendee,
+        tokens,
+        date,
+        attendee_update_tokens_operation_name \\ :attendee_update_tokens,
+        daily_tokens_fetch_operation_name \\ :daily_tokens_fetch,
+        daily_tokens_update_operation_name \\ :daily_tokens_update
+      ) do
+    date =
+      case date do
+        %DateTime{} -> DateTime.to_date(date)
+        %Date{} -> DateTime.to_date(date)
+      end
+
+    Multi.new()
+    |> Multi.update(
+      attendee_update_tokens_operation_name,
+      Attendee.changeset(attendee, %{tokens: max(attendee.tokens - tokens, 0)})
+    )
+    |> Multi.run(daily_tokens_fetch_operation_name, fn repo, _changes ->
+      {:ok,
+       repo.one(
+         from dt in DailyTokens, where: dt.attendee_id == ^attendee.id and dt.date == ^date
+       ) || %DailyTokens{date: date, attendee_id: attendee.id}}
+    end)
+    |> Multi.insert_or_update(daily_tokens_update_operation_name, fn changes ->
+      daily_tokens = Map.get(changes, daily_tokens_fetch_operation_name)
+
+      DailyTokens.changeset(daily_tokens, %{
+        tokens: if(daily_tokens.tokens, do: max(daily_tokens.tokens - tokens, 0), else: 0)
+      })
     end)
   end
 
